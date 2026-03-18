7316f7554d1fa6f2dd1c02fedf64bff3f45d0751
JunicGames <109977989+JunicGames@users.noreply.github.com>
2026-03-18 12:39:28 -0600

    Codigo (mas o menos) del arbol binario

--- ProgramaHuffman/src/Model/HeapMin.java
+++ ProgramaHuffman/src/Model/HeapMin.java
@@ -1,36 +1,65 @@
 package Model;
-
 import java.util.ArrayList;
 
 public class HeapMin {
-    private ArrayList<Integer> heap;
+    private ArrayList<Nodo> heap = new ArrayList<>();
 
-    public HeapMin() {
-        heap = new ArrayList<>();
+    public void insertar(Nodo nodo) {
+        heap.add(nodo);
+        subir(heap.size() - 1);
     }
 
-    public void insertar(int valor) {
-        heap.add(valor);
-        heapifyUp(heap.size() - 1);
+    public Nodo extraerMin() {
+        if (heap.isEmpty()) return null;
+
+        Nodo raiz = heap.get(0);
+        Nodo ultimo = heap.remove(heap.size() - 1);
+
+        if (!heap.isEmpty()) {
+            heap.set(0, ultimo);
+            bajar(0);
+        }
+
+        return raiz;
     }
 
-    private void heapifyUp(int index) {
-        int padre = (index - 1) / 2;
+    public int size() {
+        return heap.size();
+    }
 
-        while (index > 0 && heap.get(index) < heap.get(padre)) {
-            intercambiar(index, padre);
-            index = padre;
-            padre = (index - 1) / 2;
+    private void subir(int i) {
+        int padre = (i - 1) / 2;
+
+        if (i > 0 && heap.get(i).frecuencia < heap.get(padre).frecuencia) {
+            intercambiar(i, padre);
+            subir(padre);
+        }
+    }
+
+    private void bajar(int i) {
+        int izq = 2 * i + 1;
+        int der = 2 * i + 2;
+        int menor = i;
+
+        if (izq < heap.size() &&
+                heap.get(izq).frecuencia < heap.get(menor).frecuencia) {
+            menor = izq;
+        }
+
+        if (der < heap.size() &&
+                heap.get(der).frecuencia < heap.get(menor).frecuencia) {
+            menor = der;
+        }
+
+        if (menor != i) {
+            intercambiar(i, menor);
+            bajar(menor);
         }
     }
 
     private void intercambiar(int i, int j) {
-        int temp = heap.get(i);
+        Nodo temp = heap.get(i);
         heap.set(i, heap.get(j));
         heap.set(j, temp);
-    }
-
-    public void imprimir() {
-        System.out.println(heap);
     }
 }
